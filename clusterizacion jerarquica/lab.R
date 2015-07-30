db5577ca4b6ce3e434b051a74a2fc106e4ae409a
wilmeragsbit <wilmeragsgm@gmail.com>
2015-07-30 09:50:16 -0430

    adding mtcars set

--- clusterizacion jerarquica/lab.R
+++ clusterizacion jerarquica/lab.R
@@ -1,3 +1,32 @@
-datos <- data ("iris")
+## @knitr iris_set
 
-?dist
+data ("iris")
+datos <- iris
+
+## NAs by coercion for string in species
+distancia <- dist(datos) 
+dist(datos,"manhattan")
+length(dist(datos)) ## = nrow(datos)*(nrow(datos)-1)/2
+
+cluster <- hclust(distancia)
+plot(cluster)
+
+identify(cluster,function(i) print(table(iris[i,5])))
+identify(cluster,function(i) print(length(i)))
+
+
+## @knitr mtcars_set
+
+data ("mtcars")
+datos <- mtcars
+
+## NAs by coercion for string in species
+distancia <- dist(datos) 
+dist(datos,"manhattan")
+length(dist(datos)) ## = nrow(datos)*(nrow(datos)-1)/2
+
+cluster <- hclust(distancia)
+plot(cluster)
+
+identify(cluster,function(i) print(table(iris[i,5])))
+identify(cluster,function(i) print(length(i)))
